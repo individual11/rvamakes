18779ccd6a507fc32cdb4d74631e380884b809fe
Fletcher Padgett <fletcher.padgett@martinagency.com>
2013-04-15 14:05:19 -0400

    added base form handling.

--- deploy/public/js/rvamakes.js
+++ deploy/public/js/rvamakes.js
@@ -154,11 +154,19 @@ var ShowView = Backbone.View.extend({
 var EntryView = Backbone.View.extend({
     el: '#entry',
     template: Mustache.compile($('#tmplEntry').html()),
+    events:{
+        "click .addCreative":"processEntry"
+    },
     initialize: function(){
         this.render();
     },
     render:function(){
         this.$el.html(this.template());
+    },
+    processEntry:function(e){
+        e.preventDefault();
+        var $form = this.$el.find('form');
+        console.log($form.serializeArray());
     }
 });
 
