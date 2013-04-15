02a3b0a1801976c41250514725d011526828ccf4
David Vogeleer <david@individual11.com>
2013-04-15 13:16:14 -0400

    added from merge

--- deploy/public/js/rvamakes.js
+++ deploy/public/js/rvamakes.js
@@ -119,21 +119,6 @@ var ListItemView = Backbone.View.extend({
 });
 
 /* **********************************************
-     Begin AboutView.js
-********************************************** */
-
-var AboutView = Backbone.View.extend({
-    el: '#about',
-    template: Mustache.compile($('#tmplAbout').html()),
-    initialize: function(){
-        this.render();
-    },
-    render:function(){
-        this.$el.html(this.template());
-    }
-});
-
-/* **********************************************
      Begin ShowView.js
 ********************************************** */
 
@@ -144,21 +129,6 @@ var ShowView = Backbone.View.extend({
     },
     render:function(){
 
-    }
-});
-
-/* **********************************************
-     Begin EntryView.js
-********************************************** */
-
-var EntryView = Backbone.View.extend({
-    el: '#entry',
-    template: Mustache.compile($('#tmplEntry').html()),
-    initialize: function(){
-        this.render();
-    },
-    render:function(){
-        this.$el.html(this.template());
     }
 });
 
@@ -206,61 +176,6 @@ var AppView = Backbone.View.extend({
     },
     "filter:change": function (e, data) {
         console.log(data);
-    }
-});
-
-/* **********************************************
-     Begin AppRouter.js
-********************************************** */
-
-var AppRouter = Backbone.Router.extend({
-    routes:{
-        "list":"list",
-        "about": "about",
-        "entry": "entry",
-        "show/:id": "show",
-        "random": "random",
-        "filter/:tag": "filter",
-        '*path':  'defaultRoute'
-    },
-    initialize: function(){
-
-    },
-    hideSections: function () {
-        $('section').hide();
-    },
-    defaultRoute:function(){
-        this.list();
-    },
-
-    list: function () {
-        console.log('list');
-        this.hideSections();
-        $('#list').show();
-    },
-    about: function () {
-        console.log('about');
-        this.hideSections();
-        $('#about').show();
-    },
-    entry: function () {
-        console.log('entry');
-        this.hideSections();
-        $('#entry').show();
-    },
-    show: function (data) {
-        console.log('show',data);
-        this.hideSections();
-        $('#show').show();
-    },
-    random: function () {
-        console.log('random');
-        this.hideSections();
-        $('#show').show();
-    },
-    filter:function (tag){
-        console.log('filter', tag);
-        $("list");
     }
 });
 
