345486e0dc2150be594e429a2368f5e0da607ed0
Fletcher Padgett <fletcher.padgett@martinagency.com>
2013-04-16 15:24:15 -0400

    added s3 upload of images

--- deploy/routes/creative.js
+++ deploy/routes/creative.js
@@ -1,7 +1,10 @@
-var mongoose = require('mongoose');
+var AWS = require('aws-sdk'),
+    mongoose = require('mongoose'),
+    fs = require('fs');
 var Creative = mongoose.model('Creative');
 
 var queryFields = "name img url tags";
+AWS.config.loadFromPath('./config/aws.json');
 
 exports.index = function (req, res) {
     Creative.find(
@@ -21,18 +24,47 @@ exports.show = function (req, res) {
 
 exports.create = function (req, res, next) {
     console.log(req.body);
-    //console.log(req.files);
+    var img = req.files.img;
+    var filePathSegs = img.path.split('/');
+    var fileNameSegs = img.name.split('.');
+    var filename = filePathSegs[filePathSegs.length - 1] + '.' + fileNameSegs[fileNameSegs.length - 1];
+    var bucket = 'rvamakes';
+    var s3URI = 'https://s3.amazonaws.com/'+bucket+'/';
 
     var creative = req.body;
-    creative.img = "http://placehold.it/225x200&text=" + creative.name.replace(/ /g,'+');
+    creative.img = s3URI+filename;
     creative.create_at = Date.now();
 
-    new Creative(creative).save(function (err, creative, count) {
-        if (err) return next(err);
-        res.set('Content-Type', 'text/plain');
-        res.send(creative);
-    });
+    var s3 = new AWS.S3();
 
+    fs.readFile(img.path, function (err, data) {
+        if (err) {
+            console.warn(err);
+        }
+        else {
+            s3.client.putObject({
+                Bucket: bucket,
+                Key: filename,
+                Body: data,
+                ContentType: img.type,
+                ACL: 'public-read'
+
+            }, function (err, data) {
+                if (err) {
+                    console.log("s3 fail", err);
+                    res.status(response.httpResponse.statusCode).end(JSON.stringify(({err: response})));
+                }
+                else {
+                    console.log("s3 done", data);
+                    new Creative(creative).save(function (err, creative, count) {
+                        if (err) return next(err);
+                        res.set('Content-Type', 'text/plain');
+                        res.send(creative);
+                    });
+                }
+            });
+        }
+    });
 };
 
 exports.delete = function (req, res, next) {
