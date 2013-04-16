d98ab8f71548402c1c60274471e3cfdfac091b59
David Vogeleer <david@individual11.com>
2013-04-15 23:20:50 -0400

    from merger and cleanup

--- deploy/public/js/rvamakes.js
+++ deploy/public/js/rvamakes.js
@@ -126,33 +126,6 @@ var ListItemView = Backbone.View.extend({
 });
 
 /* **********************************************
-     Begin AboutView.js
-********************************************** */
-
-var AboutView = Backbone.View.extend({
-    el: '#about',
-    template: Mustache.compile($('#tmplAbout').html()),
-    initialize: function(){
-        this.render();
-    },
-    render:function(){
-        this.$el.html(this.template());
-    }
-});
-
-/* **********************************************
-     Begin ShowView.js
-********************************************** */
-
-var ShowView = Backbone.View.extend({
-    el: "#show",
-    template: Mustache.compile($("#tmplShowItem").html()),
-    renderCreative:function(json){
-        this.$el.html(this.template(json));
-    }
-});
-
-/* **********************************************
      Begin EntryView.js
 ********************************************** */
 
@@ -182,57 +155,6 @@ var EntryView = Backbone.View.extend({
             this.$el.trigger("creative:created",res);
         }
 
-
-    }
-});
-
-/* **********************************************
-     Begin AppView.js
-********************************************** */
-
-var AppView = Backbone.View.extend({
-    el: "body",
-    initialize: function () {
-        this.collections.creatives = new Creatives();
-        this.collections.creatives.fetch();
-
-        this.views.header = new AppHeaderView({});
-        this.views.footer = new AppFooterView({});
-        this.views.list = new ListView({collection: this.collections.creatives});
-        this.views.show = new ShowView({});
-        this.views.about = new AboutView({});
-        this.views.entry = new EntryView({});
-
-
-        this.router = new AppRouter();
-        Backbone.history.start();
-    },
-    render: function () {
-
-    },
-    events: {
-        "creative:create": "creative:create",
-        "creative:show": "creative:show",
-        "creative:random": "creative:random",
-        "filter:change": "filter:change"
-    },
-    collections: {},
-    views: {},
-    "creative:create": function (e, model) {
-        console.log('creative:create',model instanceof Creative,model);
-        if(model instanceof Creative){
-            this.collections.add(model);
-        }
-    },
-    "creative:show": function (e, data) {
-        var model = this.collections.creatives.findWhere({_id:data});
-        this.views.show.renderCreative(model.toJSON());
-    },
-    "creative:random": function () {
-        this.router.navigate("#/show/"+this.collections.creatives.randomId(),true);
-    },
-    "filter:change": function (e, data) {
-        console.log(data);
 
     }
 });
@@ -287,6 +209,84 @@ var AppRouter = Backbone.Router.extend({
     filter:function (tag){
         console.log('filter', tag);
         $("list");
+    }
+});
+
+/* **********************************************
+     Begin AboutView.js
+********************************************** */
+
+var AboutView = Backbone.View.extend({
+    el: '#about',
+    template: Mustache.compile($('#tmplAbout').html()),
+    initialize: function(){
+        this.render();
+    },
+    render:function(){
+        this.$el.html(this.template());
+    }
+});
+
+/* **********************************************
+     Begin ShowView.js
+********************************************** */
+
+var ShowView = Backbone.View.extend({
+    el: "#show",
+    template: Mustache.compile($("#tmplShowItem").html()),
+    renderCreative:function(json){
+        this.$el.html(this.template(json));
+    }
+});
+
+/* **********************************************
+     Begin AppView.js
+********************************************** */
+
+var AppView = Backbone.View.extend({
+    el: "body",
+    initialize: function () {
+        this.collections.creatives = new Creatives();
+        this.collections.creatives.fetch();
+
+        this.views.header = new AppHeaderView({});
+        this.views.footer = new AppFooterView({});
+        this.views.list = new ListView({collection: this.collections.creatives});
+        this.views.show = new ShowView({});
+        this.views.about = new AboutView({});
+        this.views.entry = new EntryView({});
+
+
+        this.router = new AppRouter();
+        Backbone.history.start();
+    },
+    render: function () {
+
+    },
+    events: {
+        "creative:create": "creative:create",
+        "creative:show": "creative:show",
+        "creative:random": "creative:random",
+        "filter:change": "filter:change"
+    },
+    collections: {},
+    views: {},
+    "creative:create": function (e, model) {
+        console.log('creative:create',model instanceof Creative,model);
+        if(model instanceof Creative){
+            this.collections.add(model);
+        }
+    },
+    "creative:show": function (e, data) {
+        var model = this.collections.creatives.findWhere({_id:data});
+        this.views.show.renderCreative(model.toJSON());
+    },
+    "creative:random": function () {
+        this.router.navigate("#/show/"+this.collections.creatives.randomId(),true);
+    },
+    "filter:change": function (e, data) {
+        console.log(data);
+
     }
 });
 
