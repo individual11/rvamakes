7bbdf77a1f33b7b6cd630f9cc75ab440a720911a
Fletcher Padgett <fletcher.padgett@martinagency.com>
2013-04-17 15:17:37 -0400

    cleaned up router

--- build/js/routers/AppRouter.js
+++ build/js/routers/AppRouter.js
@@ -8,9 +8,6 @@ var AppRouter = Backbone.Router.extend({
         "filter/:tag": "filter",
         '*path':  'defaultRoute'
     },
-    initialize: function(){
-
-    },
     hideSections: function () {
         $('section').hide();
         window.scrollTo(0,1);
@@ -20,23 +17,19 @@ var AppRouter = Backbone.Router.extend({
     },
 
     list: function () {
-        console.log('list');
         this.hideSections();
         $('#list').show();
 
     },
     about: function () {
-        console.log('about');
         this.hideSections();
         $('#about').show();
     },
     entry: function () {
-        console.log('entry');
         this.hideSections();
         $('#entry').show();
     },
     show: function (data) {
-        console.log('creative:show',data);
         $('body').trigger('creative:show', data);
         this.hideSections();
         $('#show').show();
@@ -45,7 +38,6 @@ var AppRouter = Backbone.Router.extend({
         $('body').trigger('creative:random');
     },
     filter:function (tag){
-        console.log('filter', tag);
         $('body').trigger('filter:change',data);
     }
 });
