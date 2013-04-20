c45fb10362bdd62937ee6d6ba786f375f1da7f47
David Vogeleer <david@individual11.com>
2013-04-20 19:15:17 -0400

    hides the options window when sections change  incase it's open

--- build/js/views/AppFooterView.js
+++ build/js/views/AppFooterView.js
@@ -1,9 +1,16 @@
 var AppFooterView = Backbone.View.extend({
     el: "footer",
+    events:{
+	  "click .inner div a": "sectionChange"
+    },
     initialize:function(){
 
     },
     render:function(){
 
+    },
+    sectionChange:function(){
+	    $('.options').hide();
     }
+    
 });
--- deploy/public/js/rvamakes.js
+++ deploy/public/js/rvamakes.js
@@ -79,12 +79,19 @@ var AppHeaderView = Backbone.View.extend({
 
 var AppFooterView = Backbone.View.extend({
     el: "footer",
+    events:{
+	  "click .inner div a": "sectionChange"
+    },
     initialize:function(){
 
     },
     render:function(){
 
+    },
+    sectionChange:function(){
+	    $('.options').hide();
     }
+    
 });
 
 /* **********************************************
