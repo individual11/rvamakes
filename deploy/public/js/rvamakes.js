f39478081f49d05f91d2b42f753a319d6d6b677e
Fletcher Padgett <fletcher.padgett@martinagency.com>
2013-04-22 11:15:31 -0400

    updated ga tracker log message, prepared package for dns

--- deploy/public/js/rvamakes.js
+++ deploy/public/js/rvamakes.js
@@ -305,11 +305,11 @@ var EntryView = Backbone.View.extend({
 var GoogleAnalyticsView = Backbone.View.extend({
     el:'#tracker',
     trackEvent:function(category, label, action, value){
-        console.log('bc.trackevent', category, label, action, value);
+        console.log('tracker.trackevent', category, label, action, value);
         //_gaq.push(['_trackEvent', category, label, action, value];
     },
     trackView:function(){
-        console.log('bc.trackview', location.pathname + location.search + location.hash);
+        console.log('tracker.trackview', location.pathname + location.search + location.hash);
        // _gaq.push(['_trackPageview', location.pathname + location.search + location.hash]);
     }
 });
